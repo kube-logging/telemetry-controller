4bc409d715f7de63fa0b1bb9283c4f3935a926c3
Kristóf Gyurácz <4431202+kristofgyuracz@users.noreply.github.com>
2024-01-05 13:27:35 +0100

    Update api/telemetry/v1alpha1/groupversion_info.go
    Co-authored-by: Szilard Parrag <szilard.parrag@gmail.com>
    Signed-off-by: Kristóf Gyurácz <4431202+kristofgyuracz@users.noreply.github.com>

--- api/telemetry/v1alpha1/groupversion_info.go
+++ api/telemetry/v1alpha1/groupversion_info.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package v1alpha1 contains API Schema definitions for the logging v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the telemetry v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=telemetry.kube-logging.dev
 package v1alpha1
