a9a111d2134740a88fbf9aea81bb526e6dd3a257
Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>
2024-03-20 07:43:32 +0100

    loki demo setup script updates
    Signed-off-by: Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>
    Signed-off-by: Kristof Gyuracz <kristof.gyuracz@axoflow.com>

--- docs/demos/loki/log-generator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-helm upgrade --install --wait log-generator oci://ghcr.io/kube-logging/helm-charts/log-generator -n example-tenant-ns
-helm upgrade --install --wait log-generator oci://ghcr.io/kube-logging/helm-charts/log-generator -n another-tenant-ns
