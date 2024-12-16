6f024cadb88f904eceba762d053fccf0595f2eeb
Bence Csati <bence.csati@axoflow.com>
2024-12-16 09:53:23 +0100

    chore: make operator args configurable via chart values
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- cmd/main.go
+++ cmd/main.go
@@ -65,7 +65,7 @@ func main() {
 	var secureMetrics bool
 	var enableHTTP2 bool
 	var tlsOpts []func(*tls.Config)
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8443", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
