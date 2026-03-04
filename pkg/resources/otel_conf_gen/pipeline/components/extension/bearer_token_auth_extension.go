3fcd6645e8fbb3f71825f2911eaddbc54681eaaf
Bence Csati <bence.csati@axoflow.com>
2026-03-04 15:59:49 +0100

    feat: hide secrets from config, mount as env-var
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- pkg/resources/otel_conf_gen/pipeline/components/extension/bearer_token_auth_extension.go
+++ pkg/resources/otel_conf_gen/pipeline/components/extension/bearer_token_auth_extension.go
@@ -14,7 +14,12 @@
 
 package extension
 
-import "github.com/kube-logging/telemetry-controller/pkg/resources/otel_conf_gen/pipeline/components"
+import (
+	"fmt"
+
+	"github.com/kube-logging/telemetry-controller/api/telemetry/v1alpha1"
+	"github.com/kube-logging/telemetry-controller/pkg/resources/otel_conf_gen/pipeline/components"
+)
 
 const defaultBearerAuthTokenField = "token"
 
@@ -22,7 +27,26 @@ type BearerTokenAuthExtensionConfig struct {
 	BearerToken string `json:"token,omitempty"`
 }
 
+// BearerAuthEnvVarName returns the env var name for bearerauth token
+// for the given output namespace and name.
+func BearerAuthEnvVarName(namespacedName v1alpha1.NamespacedName) string {
+	return fmt.Sprintf("TC_BEARERAUTH_%s_%s_TOKEN", sanitizeEnvVarSegment(namespacedName.Namespace), sanitizeEnvVarSegment(namespacedName.Name))
+}
+
+// BearerAuthSecretKey returns the secret data key for bearerauth token.
+func BearerAuthSecretKey(namespacedName v1alpha1.NamespacedName) string {
+	return fmt.Sprintf("bearerauth_%s_%s_token", sanitizeSecretKeySegment(namespacedName.Namespace), sanitizeSecretKeySegment(namespacedName.Name))
+}
+
 func GenerateBearerAuthExtensionsForOutput(output components.OutputWithSecretData) BearerTokenAuthExtensionConfig {
+	return BearerTokenAuthExtensionConfig{
+		BearerToken: fmt.Sprintf("${%s}", BearerAuthEnvVarName(output.Output.NamespacedName())),
+	}
+}
+
+// BearerAuthSecretData returns the secret data map entries for bearer token credentials
+// extracted from the output's secret data.
+func BearerAuthSecretData(output components.OutputWithSecretData) map[string][]byte {
 	var effectiveTokenField string
 	if output.Output.Spec.Authentication.BearerAuth.TokenField != "" {
 		effectiveTokenField = output.Output.Spec.Authentication.BearerAuth.TokenField
@@ -30,10 +54,10 @@ func GenerateBearerAuthExtensionsForOutput(output components.OutputWithSecretDat
 		effectiveTokenField = defaultBearerAuthTokenField
 	}
 
-	config := BearerTokenAuthExtensionConfig{}
+	data := make(map[string][]byte)
 	if t, ok := output.Secret.Data[effectiveTokenField]; ok {
-		config.BearerToken = string(t)
+		data[BearerAuthSecretKey(output.Output.NamespacedName())] = t
 	}
 
-	return config
+	return data
 }
