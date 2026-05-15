c7834de620838ecdc013fdeb40f2a7b926520c71
Bence Csati <bence.csati@axoflow.com>
2026-05-15 18:22:32 +0200

    chore: bump axoflow-otelcollector
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- pkg/resources/manager/collector_manager.go
+++ pkg/resources/manager/collector_manager.go
@@ -47,7 +47,7 @@ import (
 
 const (
 	otelCollectorKind            = "OpenTelemetryCollector"
-	axoflowOtelCollectorImageRef = "ghcr.io/axoflow/axoflow-otel-collector/axoflow-otel-collector:0.150.0-axoflow.2"
+	axoflowOtelCollectorImageRef = "ghcr.io/axoflow/axoflow-otel-collector/axoflow-otel-collector:0.152.0-axoflow.0"
 )
 
 var (
